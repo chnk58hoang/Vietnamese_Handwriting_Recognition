52b35acd7adf2062d840092490d41ab357a179dc
chnk58hoang <19021285@vnu.edu.vn>
2022-06-28 14:48:14 +0700

    cleancode

--- network/model.py
+++ network/model.py
@@ -8,13 +8,12 @@ import torch.nn as nn
 class VietOCRVGG16(nn.Module):
     def __init__(self, finetune=False):
         super(VietOCRVGG16, self).__init__()
-        resnet = resnet50(pretrained=True)
-        self.backbone = nn.Sequential(*(list(resnet.children())[:-2]))
+        self.backbone = vgg16(pretrained=True)
 
         for param in self.backbone.parameters():
             param.requires_grad = finetune
-        self.fc1 = nn.Linear(10240, 2048)
-        self.fc2 = nn.Linear(2048,512)
+
+        self.fc1 = nn.Linear(2560,512)
         self.bi_lstm = nn.LSTM(512, 256, batch_first=True, bidirectional=True)
         self.fc3 = nn.Linear(512, num_letters)
         self.loss_fn = nn.CTCLoss(blank=0)
@@ -24,8 +23,6 @@ class VietOCRVGG16(nn.Module):
         x = x.permute(0, 3, 1, 2)
         x = x.reshape(x.size(0), x.size(1), -1)
         x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
         x = F.relu(x)
         x, _ = self.bi_lstm(x)
         x = self.fc3(x)
--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import torch
-from torchvision.models import resnet50
+from torchvision.models import resnet50,vgg19
 import torch.nn as nn
 
 
-model = resnet50(pretrained=False)
-newmodel = nn.Sequential(*(list(model.children())[:-2]))
-print(newmodel)
+x  = torch.rand(1,3,160,2560)
 
-x = torch.rand(1,3,160,2560)
-
-print(newmodel(x).size())
+model = vgg19()
+print(model.features(x).size())
