8581f0fa5bcc4c6c1c613452eff044061e46f0dd
chnk58hoang <19021285@vnu.edu.vn>
2022-07-05 22:07:49 +0700

    add finetune mode

--- finetune.py
+++ finetune.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
 
 
     for epoch in range(args.epoch):
-        print(f"Epoch {epoch+1} / {args.epoch}")
+        print(f"Epoch {epoch + 1} / {args.epoch}")
         train_loss = train_model(model, device, train_dataset, train_dataloader, optimizer)
         print(f'Training loss:{train_loss}')
         val_loss = valid_model(model, device, valid_dataset, valid_dataloader)
